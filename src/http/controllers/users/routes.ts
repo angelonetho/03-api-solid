ccb1d41d50e653f3fdb2d98c53197316d572a4a8
Angelo Netho <angelonetho@gmail.com>
2024-02-15 16:35:18 -0300

    Refresh Token & RBAC

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,14 +1,17 @@
 import { FastifyInstance } from 'fastify'
 
+import { verifyJWT } from '../../middlewares/verify-jwt'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
+import { refresh } from './refresh'
 import { register } from './register'
-import { verifyJWT } from '../../middlewares/verify-jwt'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
 
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
